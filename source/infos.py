d802304f9340bc6d0672f7822838a07763e21f5e
NoeSchaller <tamialunaire@gmail.com>
2022-12-21 15:38:32 +0000

    change info personelles

--- source/infos.py
+++ source/infos.py
@@ -1,15 +1,15 @@
 class DocumentInfos:
 
     title = u'Programmation dynamique'
-    first_name = 'Cédric'
-    last_name = 'Donner'
+    first_name = 'Noé'
+    last_name = 'Schaller'
     author = f'{first_name} {last_name}'
-    year = u'2022'
-    month = u'Juillet'
+    year = u'2023'
+    month = u'Janvier'
     seminary_title = u'Algorithmes et structures de données II'
     tutor = u"Cédric Donner"
     release = "(Version finale)"
-    repository_url = "https://github.com/donnerc/prog-dynamique"
+    repository_url = "https://github.com/NoeSchaller/prog-dynamique"
 
     @classmethod
     def date(cls):
